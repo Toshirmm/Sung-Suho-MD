e3e5f53ba328c80ecb711b3d0e92e7343fa911ca
Toshirmm <144406472+Toshirmm@users.noreply.github.com>
2026-01-01 12:22:48 -0300

    Change default SESSION_ID and OWNER_NAME
    Updated default values for SESSION_ID and OWNER_NAME.

--- config.js
+++ config.js
@@ -7,9 +7,9 @@ function convertToBool(text, fault = "true") {
 }
 
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "",
+  SESSION_ID: process.env.SESSION_ID || "zRtFyKDB#1hfOW-gjhvqhneIjzBUT0RdfG5oQ7hestor0YCXoKFI",
   OWNER_NUM: process.env.OWNER_NUM || "27649342626",
-  OWNER_NAME: process.env.OWNER_NAME || "Lord Sung",
+  OWNER_NAME: process.env.OWNER_NAME || "woody",
   REPO: process.env.REPO || "https://github.com/NaCkS-ai/Sung-Suho-MD", 
   PREFIX: process.env.PREFIX || ".",
   AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
